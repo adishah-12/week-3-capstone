80a170e2dd52fe302b2470b018392d862a4f5e19
adishah-12 <contact@adi.moe>
2026-09-21 09:20:49 -0400

    Reworked connection strings for EB env

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -23,7 +23,7 @@ if (builder.Environment.IsDevelopment())
 }
 else
 {
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var connectionString = builder.Configuration.GetConnectionString("UserDb");
     builder.Services.AddDbContext<UserServiceContext>(options =>
         options.UseNpgsql(connectionString));
 }
